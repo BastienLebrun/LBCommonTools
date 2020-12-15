b308578790cc1238fdb9961bab8f35f9005574dd
Bastien Lebrun <lbastien974@gmail.com>
2020-12-15 23:51:29 +0100

    Initial Commit

---
+++ LBCommonTools/LBCommonTools.h
@@ -0,0 +1,17 @@
+//
+//
+// LBCommonTools.h, created on 15/12/2020 by Bastien LEBRUN
+// 
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LBCommonTools.
+FOUNDATION_EXPORT double LBCommonToolsVersionNumber;
+
+//! Project version string for LBCommonTools.
+FOUNDATION_EXPORT const unsigned char LBCommonToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LBCommonTools/PublicHeader.h>
+
+
